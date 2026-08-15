101864ea84830de11c0413b65f6343aafe7d7da4
leandrocodegit <lpoliveira.ti@gmail.com>
2026-08-15 13:42:29 -0300

    Salvando Bipe

--- ios/OwnTracks/OwnTracks/ViewController.swift
+++ ios/OwnTracks/OwnTracks/ViewController.swift
@@ -1708,10 +1708,10 @@ struct BipeEmergencyIntent: AppIntent {
         if let data = try? JSONSerialization.data(withJSONObject: payload, options: []) {
             // Alta prioridade: QoS 2 (ExactlyOnce), não retido
             if let qos = MQTTQosLevel(rawValue: UInt8(2)) {
-                delegate.connection?.sendData(data, topic: bipeTopic, topicAlias: nil, qos: qos, retain: false)
+                delegate.connection?.send(data, topic: bipeTopic, topicAlias: nil, qos: qos, retain: false)
             } else {
                 // Fallback caso enum não mapeie bem
-                delegate.connection?.sendData(data, topic: bipeTopic, topicAlias: nil, qos: MQTTQosLevelExactlyOnce, retain: false)
+                delegate.connection?.send(data, topic: bipeTopic, topicAlias: nil, qos: MQTTQosLevelExactlyOnce, retain: false)
             }
         }
         
