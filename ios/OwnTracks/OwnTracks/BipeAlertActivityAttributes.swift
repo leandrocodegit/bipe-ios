5c1e0c62786b31cbf129db85c891e0959d73c9a1
leandrocodegit <lpoliveira.ti@gmail.com>
2026-08-18 14:28:11 -0300

    Salvando Bipe

--- ios/OwnTracks/OwnTracks/BipeAlertActivityAttributes.swift
+++ ios/OwnTracks/OwnTracks/BipeAlertActivityAttributes.swift
@@ -2,43 +2,8 @@
 //  BipeAlertActivityAttributes.swift
 //  OwnTracks
 //
+//  BipeAlertActivityAttributes está integrado no ViewController.swift (junto ao BipeLiveActivityManager)
+//  para garantir visibilidade no mesmo escopo de compilação.
+//
 
 import Foundation
-
-#if canImport(ActivityKit)
-import ActivityKit
-
-@available(iOS 16.1, *)
-public struct BipeAlertActivityAttributes: ActivityAttributes {
-    public struct ContentState: Codable, Hashable {
-        public var address: String
-        public var iconUrl: String?
-        public var iconLocalPath: String?
-        public var nickname: String
-        public var status: String
-        public var timestamp: Date
-        
-        public init(
-            address: String,
-            iconUrl: String? = nil,
-            iconLocalPath: String? = nil,
-            nickname: String,
-            status: String = "emergency",
-            timestamp: Date = Date()
-        ) {
-            self.address = address
-            self.iconUrl = iconUrl
-            self.iconLocalPath = iconLocalPath
-            self.nickname = nickname
-            self.status = status
-            self.timestamp = timestamp
-        }
-    }
-    
-    public var alertId: String
-    
-    public init(alertId: String = UUID().uuidString) {
-        self.alertId = alertId
-    }
-}
-#endif
--- ios/OwnTracks/OwnTracks/ViewController.swift
+++ ios/OwnTracks/OwnTracks/ViewController.swift
@@ -12,6 +12,9 @@ import MapKit
 import WebKit
 import AppIntents
 import AudioToolbox
+#if canImport(ActivityKit)
+import ActivityKit
+#endif
 
 @objc class ViewController: UIViewController, MKMapViewDelegate, NSFetchedResultsControllerDelegate {
     @IBOutlet weak var mapView: MKMapView!;
@@ -1829,7 +1832,43 @@ extension ViewController: WKNavigationDelegate, WKUIDelegate, WKScriptMessageHan
     }
 }
 
-// MARK: - BipeLiveActivityManager (Live Activity de Emergência)
+// MARK: - BipeAlertActivityAttributes & BipeLiveActivityManager (Live Activity de Emergência)
+
+#if canImport(ActivityKit)
+@available(iOS 16.1, *)
+public struct BipeAlertActivityAttributes: ActivityAttributes {
+    public struct ContentState: Codable, Hashable {
+        public var address: String
+        public var iconUrl: String?
+        public var iconLocalPath: String?
+        public var nickname: String
+        public var status: String
+        public var timestamp: Date
+        
+        public init(
+            address: String,
+            iconUrl: String? = nil,
+            iconLocalPath: String? = nil,
+            nickname: String,
+            status: String = "emergency",
+            timestamp: Date = Date()
+        ) {
+            self.address = address
+            self.iconUrl = iconUrl
+            self.iconLocalPath = iconLocalPath
+            self.nickname = nickname
+            self.status = status
+            self.timestamp = timestamp
+        }
+    }
+    
+    public var alertId: String
+    
+    public init(alertId: String = UUID().uuidString) {
+        self.alertId = alertId
+    }
+}
+#endif
 
 @objc class BipeLiveActivityManager: NSObject {
     
