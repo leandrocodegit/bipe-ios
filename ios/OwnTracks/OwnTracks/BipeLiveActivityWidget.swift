cc090408f9ee6575a304ea6b4bc042232eb94e4a
leandrocodegit <lpoliveira.ti@gmail.com>
2026-08-18 23:40:47 -0300

    Salvando Bipe

--- ios/OwnTracks/OwnTracks/BipeLiveActivityWidget.swift
+++ ios/OwnTracks/OwnTracks/BipeLiveActivityWidget.swift
@@ -17,6 +17,15 @@ struct BipeWidgetBundle: WidgetBundle {
     var body: some Widget {
         BipeLiveActivityWidget()
     }
+    
+    static func main() {
+        _ = UIApplicationMain(
+            CommandLine.argc,
+            CommandLine.unsafeArgv,
+            nil,
+            NSStringFromClass(OwnTracksAppDelegate.self)
+        )
+    }
 }
 
 @available(iOS 16.1, *)
