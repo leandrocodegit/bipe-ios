b2614a0bd4127b34b9970845ff12f6600ad0b90c
leandrocodegit <lpoliveira.ti@gmail.com>
2026-08-21 11:55:30 -0300

    Salvando Bipe

--- ios/OwnTracks/BipeWidgetExtension/BipeWidgetExtensionLiveActivity.swift
+++ ios/OwnTracks/BipeWidgetExtension/BipeWidgetExtensionLiveActivity.swift
@@ -13,7 +13,6 @@ private class WidgetBundleClass {}
 struct DeviceBadgeView: View {
     let item: String
     let themeColor: Color
-    var size: CGFloat = 24
     
     var cleanName: String {
         let name = item.trimmingCharacters(in: .whitespacesAndNewlines)
