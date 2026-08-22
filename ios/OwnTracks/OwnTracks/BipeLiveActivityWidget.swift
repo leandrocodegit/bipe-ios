8ecf466b5ca2046e50b7aad5af0665e31d55c401
leandrocodegit <lpoliveira.ti@gmail.com>
2026-08-22 16:50:54 -0300

    Salvando Bipe

--- ios/OwnTracks/OwnTracks/BipeLiveActivityWidget.swift
+++ ios/OwnTracks/OwnTracks/BipeLiveActivityWidget.swift
@@ -687,7 +687,7 @@ public struct BipeLiveActivityWidget: Widget {
                         .padding(.vertical, 4)
                         .background(Capsule().fill(themeColor))
                         .foregroundColor(.white)
-                    } else if isTransition {
+                    } else if isTransition && !isActive {
                         HStack(spacing: 4) {
                             Image(systemName: isExit ? "arrow.left.to.line.compact" : "arrow.right.to.line.compact")
                                 .font(.system(size: 10, weight: .bold))
